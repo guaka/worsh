673dfee7a2a06638d0f98593294f05835c1c63ba
Kasper Souren <kasper@guaka.org>
2013-09-11 12:33:03 +0200

    sql-dump!

--- worsh.php
+++ worsh.php
@@ -1,7 +1,7 @@
 <?php
 require_once 'wp-config.php';
-require_once 'wp-settings.php';
 
+// not sure where the newline is coming from
 
 class ArgParser {
 
@@ -10,7 +10,7 @@ class ArgParser {
   }
 
   function command() {
-    return $this->argv[1];
+    return str_replace('-', '_', $this->argv[1]);
   }
   
   function params() { 
@@ -26,9 +26,33 @@ $cmd = $a->command();
 
 if (function_exists('worsh_' . $cmd)) {
   call_user_func('worsh_' . $cmd, $a->params());
+} else {
+  print "no command found, TODO: add manual\n";
 }
 
 
 function worsh_oget($params) {
   print get_option($params[0]) . "\n";
+}
+
+function mysql_params() {
+  return DB_NAME." --host=".DB_HOST." --user=".DB_USER." --password=".DB_PASSWORD."\n";
+}
+
+function worsh_sql_connect($params) {
+  print 'mysql --database=' . mysql_params();
+}
+
+function worsh_exec($command) {
+  print "executing $command\n";
+  passthru($command);
+}
+
+function worsh_sql_cli() {
+  print "This doesn't work as expected, yet\n";
+  worsh_exec('mysql --database=' . mysql_params());
+}
+
+function worsh_sql_dump() {
+  worsh_exec('mysqldump ' . mysql_params());
 }
